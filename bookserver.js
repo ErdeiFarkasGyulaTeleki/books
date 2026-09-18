dbed57af7de526c09cf1a0eedf7a90a9e3ac01c6
ErdeiFarkasGyulaTeleki <erdei.farkas.2022d@telekimezotur.hu>
2026-09-18 08:42:31 +0200

    id alapján

--- bookserver.js
+++ bookserver.js
@@ -22,7 +22,22 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("Üdv!")
-})
+});
+
+app.get('/api/books/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const [rows] = await pool.query('SELECT * FROM books WHERE id = ?', [id]);
+        if (rows.length === 0) {
+            return res.status(404).json({ error: 'Product not found' });
+        }
+        res.json(rows[0]);
+    } catch (error) {
+        console.error("Error fetching books: ", error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 
 app.get('/api/books', async (req, res) => {
     try {
